3199ff12220edc8bf402cdf5cd2a41bfd336b57d
Rhys Saldanha <rhys-saldanha@gmail.com>
2023-10-22 16:27:21 +0100

    Add local test application with kafka containers

---
+++ src/test/java/org/rhyssaldanha/springreactivekafka/TestApplication.java
@@ -0,0 +1,13 @@
+package org.rhyssaldanha.springreactivekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.from(SpringReactiveKafkaApplication::main)
+                .with(TestApplication.class)
+                .run(args);
+    }
+}
