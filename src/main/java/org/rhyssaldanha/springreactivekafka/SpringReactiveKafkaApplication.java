a557a7de80688bbeb88bb687f901959db62a6afe
Rhys Saldanha <rhys-saldanha@gmail.com>
2023-10-22 16:26:03 +0100

    Initial project from Spring Initialiser

---
+++ src/main/java/org/rhyssaldanha/springreactivekafka/SpringReactiveKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.rhyssaldanha.springreactivekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rhyssaldanha/springreactivekafka/SpringReactiveKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rhyssaldanha.springreactivekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
